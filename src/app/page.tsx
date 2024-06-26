81f00f1f719318d0da9ca500500bb58526efb3ed
Pavel Peroutka <pavel.peroutka@ataccama.com>
2024-06-26 13:33:33 +0200

    feat: display logged user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ import {
 import { useState } from 'react'
 import { tasks } from './dummyTasks'
 import { Task } from './types'
-import { signIn } from '@/auth'
-import { SignIn, signInAction } from './_components/SignInButton'
+
+import { SessionProvider, signIn, useSession } from 'next-auth/react'
 
 const TaskDialog = ({
   task,
@@ -48,27 +48,33 @@ const NewTaskBar = () => {
   return <div className="card card-hover">vytvorit poznamku</div>
 }
 
+const AuthStatus = () => {
+  const session = useSession()
+
+  return <div>{session.data?.user?.email}</div>
+}
+
 export default function Home() {
   const [openedTask, setOpenedTask] = useState<Task | undefined>()
   const closeDialog = () => setOpenedTask(undefined)
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form action={signInAction}>
-        <button type="submit">Signin with Github</button>
-      </form>
-      <NewTaskBar />
-      <div className="max-w-xl">
-        {tasks.map((task) => (
-          <TaskCard
-            task={task}
-            key={task.id}
-            onClick={() => setOpenedTask(task)}
-          />
-        ))}
-      </div>
-
-      <TaskDialog task={openedTask} onClose={closeDialog} />
-    </main>
+    <SessionProvider>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <button onClick={() => signIn()}>Sign in</button>
+        <AuthStatus />
+        <NewTaskBar />
+        <div className="max-w-xl">
+          {tasks.map((task) => (
+            <TaskCard
+              task={task}
+              key={task.id}
+              onClick={() => setOpenedTask(task)}
+            />
+          ))}
+        </div>
+        <TaskDialog task={openedTask} onClose={closeDialog} />
+      </main>
+    </SessionProvider>
   )
 }
