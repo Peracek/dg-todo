fd27c44ddbd924191e2c56656e909c2d98faf86e
Pavel Peroutka <pavel.peroutka@ataccama.com>
2024-06-26 14:13:20 +0200

    refactor: server-side auth

--- src/app/_components/SignInButton.tsx
+++ src/app/_components/SignInButton.tsx
@@ -1,6 +1,14 @@
-'use server'
 import { signIn } from '@/auth'
 
-export const signInAction = async () => {
-  await signIn('github')
+export function SignInButton() {
+  return (
+    <form
+      action={async () => {
+        'use server'
+        await signIn('github')
+      }}
+    >
+      <button type="submit">Signin with GitHub</button>
+    </form>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import prisma from '../db/prismaClient'
 
 import { signIn, useSession } from 'next-auth/react'
 import { TaskList } from './_components/TaskList'
+import { auth } from '@/auth'
+import { SignInButton } from './_components/SignInButton'
 
 const NewTaskBar = () => {
   return <div className="card card-hover">vytvorit poznamku</div>
@@ -15,12 +17,13 @@ const AuthStatus = () => {
 
 export default async function Home() {
   const tasks = await prisma.task.findMany()
+  const session = await auth()
 
   return (
     // <SessionProvider>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <button onClick={() => signIn()}>Sign in</button> */}
-      {/* <AuthStatus /> */}
+      <SignInButton />
+      <div>hello {session?.user?.name}</div>
       <NewTaskBar />
       <div className="max-w-xl">
         <TaskList tasks={tasks} />
