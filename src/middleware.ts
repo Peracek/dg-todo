a4c79202033ef9b373a5488f0c003b634d572a3d
Pavel Peroutka <pavel.peroutka@ataccama.com>
2024-06-27 22:52:38 +0200

    feat: always sign in

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,13 @@
-export { auth as middleware } from '@/auth'
+import { auth } from '@/auth'
+import { NextResponse } from 'next/server'
+
+export default auth((req) => {
+  if (!req.auth) {
+    return NextResponse.redirect(new URL('api/auth/signin', req.url))
+  }
+  return NextResponse.next()
+})
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+}
