b00b9bf2aa64e45c6eb33b7d000d43c08616437d
Pavel Peroutka <pavel.peroutka@ataccama.com>
2024-06-27 09:57:35 +0200

    feat: working task title addition

--- src/app/_components/NewTaskFloatBar.tsx
+++ src/app/_components/NewTaskFloatBar.tsx
@@ -1,25 +1,31 @@
 'use client'
 
 import { useTasks } from '@/db/Store'
-import { Button } from '@headlessui/react'
+import { Button, Input } from '@headlessui/react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+type Inputs = { title: string }
 
 export const NewTaskFloatBar = () => {
+  const { register, handleSubmit, reset } = useForm<Inputs>()
   const { addTask } = useTasks()
+  const handleAdd: SubmitHandler<Inputs> = (data) => {
+    addTask({
+      id: crypto.randomUUID(),
+      title: data.title,
+      description: 'test description',
+      authorId: null,
+    })
+    reset()
+  }
 
   return (
-    <div className="card card-hover">
-      <Button
-        onClick={() =>
-          addTask({
-            id: crypto.randomUUID(),
-            title: 'test title',
-            description: 'test description',
-            authorId: null,
-          })
-        }
-      >
-        vytvorit poznamku
-      </Button>
-    </div>
+    <form className="card card-hover" onSubmit={handleSubmit(handleAdd)}>
+      <Input
+        placeholder="Create new task..."
+        className="outline-none"
+        {...register('title')}
+      />
+    </form>
   )
 }
