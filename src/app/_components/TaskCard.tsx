bbfa12d88f6d8108971c02a41f379498f83518f5
Pavel Peroutka <pavel.peroutka@ataccama.com>
2024-06-28 10:44:20 +0200

    feat: possible to unmark task as done

--- src/app/_components/TaskCard.tsx
+++ src/app/_components/TaskCard.tsx
@@ -1,5 +1,7 @@
 import { MdCheckCircleOutline } from 'react-icons/md'
 import { Task } from '@prisma/client'
+import { useTasks } from '@/db/StoreContext'
+import { MouseEventHandler } from 'react'
 
 export const TaskCard = ({
   task,
@@ -7,22 +9,35 @@ export const TaskCard = ({
 }: {
   task: Task
   onClick: () => void
-}) => (
-  <div
-    role="button"
-    onClick={onClick}
-    className="card hover:card-hover flex gap-4 items-center"
-  >
-    <button>
-      <MdCheckCircleOutline className="w-5 h-5 opacity-25 hover:opacity-100" />
-    </button>
-    <div className="whitespace-nowrap overflow-hidden">
-      <p className="mb-2 tracking-tight text-gray-900 overflow-hidden text-ellipsis">
-        {task.title}
-      </p>
-      <p className="text-gray-700 text-sm overflow-hidden text-ellipsis">
-        {task.description}
-      </p>
+}) => {
+  const { updateTask } = useTasks()
+  const handleFinished: MouseEventHandler = (e) => {
+    e.stopPropagation()
+    updateTask({ ...task, isCompleted: !task.isCompleted })
+  }
+
+  const buttonOpracityStyle = task.isCompleted
+    ? 'opacity-100'
+    : 'opacity-25 hover:opacity-100'
+  const strikedStyle = task.isCompleted ? 'line-through' : ''
+
+  return (
+    <div
+      role="button"
+      onClick={onClick}
+      className={`card hover:card-hover flex gap-4 items-center ${strikedStyle}`}
+    >
+      <button onClick={handleFinished}>
+        <MdCheckCircleOutline className={`text-lg ${buttonOpracityStyle}`} />
+      </button>
+      <div className="whitespace-nowrap overflow-hidden">
+        <p className="mb-2 tracking-tight text-gray-900 overflow-hidden text-ellipsis">
+          {task.title}
+        </p>
+        <p className="text-gray-700 text-sm overflow-hidden text-ellipsis">
+          {task.description}
+        </p>
+      </div>
     </div>
-  </div>
-)
+  )
+}
